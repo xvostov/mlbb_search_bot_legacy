cdfdde41b442f509c1c7b4ddddcdbd1a2b6c3503
xvost <konstantine.zam@yandex.ru>
2023-03-05 00:43:50 +0300

    Добавил импорт handlers в app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from aiogram import executor
 from loader import dp, loop
 from postman import distribution_iteration
-
+import handlers
 
 if __name__ == '__main__':
     loop.create_task(distribution_iteration())
