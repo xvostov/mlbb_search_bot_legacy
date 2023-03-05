64e92785b17540151da25ac7264fd5985a2e99a1
xvost <konstantine.zam@yandex.ru>
2023-03-05 17:14:47 +0300

    Сделал фикс мелких багов

--- postman.py
+++ postman.py
@@ -7,58 +7,62 @@ from utils import render_msg
 from loguru import logger
 from aiogram.utils import exceptions
 
+
 async def send_notification(recipient, profile):
     kb = await get_like_dislike_kb(recipient.user_id, profile.user_id)
     await bot.send_message(recipient.user_id.strip(), render_msg(profile),
                            reply_markup=kb)
 
+
 async def distribution_iteration():
     while True:
-        profiles = await db.functions.get_all_profiles()
+        try:
+            profiles = await db.functions.get_all_profiles()
 
+            for profile in profiles:
+                for recipient in profiles:
 
-        for profile in profiles:
-            for recipient in profiles:
+                    observered_user_ids = [view.observed_user_id for view in recipient.views]
 
-                observered_user_ids = [view.observed_user_id for view in recipient.views]
-
-                if recipient.user_id == profile.user_id:
-                    continue
-
-                elif profile.user_id in observered_user_ids:
-                    continue
-
-                if recipient.user_filter:
-                    user_filter = recipient.user_filter
-
-                    if profile.current_rank < user_filter.min_rank:
-                        continue
-                    elif profile.current_win_rate < user_filter.min_win_rate:
-                        continue
-                    elif profile.voice_communication != user_filter.voice_communication:
+                    if recipient.user_id == profile.user_id:
                         continue
 
-                    if user_filter.min_pts:
-                        if not profile.current_pts:
+                    elif profile.user_id in observered_user_ids:
+                        continue
+
+                    if recipient.user_filter:
+                        user_filter = recipient.user_filter
+
+                        if profile.current_rank < user_filter.min_rank:
                             continue
-                        else:
-                            if profile.current_pts < user_filter.min_pts:
+                        elif profile.current_win_rate < user_filter.min_win_rate:
+                            continue
+                        elif profile.voice_communication != user_filter.voice_communication:
+                            continue
+
+                        if user_filter.min_pts:
+                            if not profile.current_pts:
                                 continue
+                            else:
+                                if profile.current_pts < user_filter.min_pts:
+                                    continue
 
+                    try:
+                        logger.debug(f'[{recipient.user_id}] отправляю анкету {profile.user_id}')
+                        await send_notification(recipient, profile)
 
-                try:
-                    logger.debug(f'[{recipient.user_id}] отправляю анкету {profile.user_id}')
-                    await send_notification(recipient, profile)
+                    except exceptions.BotBlocked:
+                        logger.debug(f'[{recipient.user_id}] заблокировал бота')
 
-                except exceptions.BotBlocked:
-                    logger.debug(f'[{recipient.user_id}] заблокировал бота')
+                    except Exception as err:
+                        logger.error(f'[{recipient.user_id}] {err}')
+                    else:
+                        logger.info(f'[{recipient.user_id}] успешно отправил анкету {profile.user_id}')
+                        await db.functions.add_to_viewed(recipient.user_id, profile.user_id)
 
-                except Exception as err:
-                    logger.error(f'[{recipient.user_id}] {err}')
-                else:
-                    logger.info(f'[{recipient.user_id}] успешно отправил анкету {profile.user_id}')
-                    await db.functions.add_to_viewed(recipient.user_id, profile.user_id)
+                    await asyncio.sleep(1)
 
-                await asyncio.sleep(1)
+            await asyncio.sleep(20)
 
-    await asyncio.sleep(20)
+        except Exception as err:
+            logger.error(err)
