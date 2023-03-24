53ecb4ff11034449c165ea5e1950389203943665
xvost <konstantine.zam@yandex.ru>
2023-03-24 21:15:58 +0300

    Удалил dotenv

--- settings.py
+++ settings.py
@@ -1,9 +1,5 @@
 import os
 
-# Удалить!
-from dotenv import load_dotenv
-load_dotenv('D:\Projects\mlbb_bot\.env')
-
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 PROM_EXP_PORT = int(os.environ.get('PROM_EXP_PORT'))
 DB_PASSWORD = os.environ.get('MYSQL_ROOT_PASSWORD')
